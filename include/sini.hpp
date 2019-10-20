0938451b9ef4bfea86985bc7a6a8f977e01f54a2
Jeramy Harrison <dbralir@gmail.com>
2019-10-19 22:26:11 -0500

    Added documentation comments to the parse function

--- include/sini.hpp
+++ include/sini.hpp
@@ -96,6 +96,8 @@ namespace sini
             namespace x3 = boost::spirit::x3;
 
             using x3::rule;
+
+            // X3 built-in parsers
             using x3::char_;
             using x3::space;
             using x3::eps;
@@ -106,42 +108,63 @@ namespace sini
 
             using boost::fusion::at_c;
 
+            // pointer to current section (used in the setProp semantic action)
             auto curSection = &addSection("");
 
-            auto concat = [](auto& ab){ return at_c<0>(ab) + at_c<1>(ab); };
-
+            // semantic action: begins a new section
             auto startSection = [&](auto& ctx){ curSection = &addSection(_attr(ctx)); };
+
+            // semantic action: sets a property on the current section
             auto setProp = [&](auto& ctx){ curSection->set(at_c<0>(_attr(ctx)), at_c<1>(_attr(ctx))); };
 
+            // whitepsace
             auto ws = char_(" \t");
+            // whitespace including newline
             auto wsn = ws | eol;
 
+            // string enclosed in single quotes, value doesn't include the quotes
             auto singleQuotedValue = '\'' >> *char_ > '\'';
+            // string enclosed in double quotes, value doesn't include the quotes
             auto doubleQuotedValue = '"' >> *char_ > '"';
 
+            // a chunk of text which doesn't contain whitespace
             auto textChunk = +(char_ - wsn);
+
+            // a sequence of textChunks separated by whitespace
             auto rawValue = textChunk >> *(+ws >> textChunk);
 
+            // property key name
             auto propKey
                 = rule<struct propKey, std::string>{"propKey"}
                 = lexeme[ char_("a-zA-Z.$:") > *char_("a-zA-Z0-9._~\\-$: ") ];
 
+            // property value
             auto propValue
                 = rule<struct propValue, std::string>{"propValue"}
                 = lexeme[ singleQuotedValue | doubleQuotedValue | rawValue | eps ];
 
+            // section header, semantic value doesn't include the brackets
             auto sectionHeader = '[' > +(char_ - ']') > ']';
+
+            // property definition
             auto property = propKey > '=' > propValue;
+
+            // comment
             auto comment = ';' > *(char_ - eol);
 
+            // a line containing either a section header or property definition (or nothing), and an optional comment
             auto line = -(sectionHeader[startSection] | property[setProp]) >> -comment;
 
+            // whole INI file
             auto iniFile
                 = rule<_detail::error_handler>{"iniFile"}
                 = line % eol;
 
+            // current iterator into ini, should be equal to enditer when done
             auto iter = begin(ini);
-            auto enditer = end(ini);
+            // end of ini file
+            const auto enditer = end(ini);
+            // output stream for errors, used to form exception messages
             auto err = std::ostringstream{};
 
             using x3::phrase_parse;
@@ -149,12 +172,16 @@ namespace sini
             using x3::error_handler_tag;
             using error_handler_type = x3::error_handler<std::string::const_iterator>;
 
+            // default error handler
             auto error_handler = error_handler_type(iter, enditer, err);
 
+            // top-level parser, wraps iniFile with error handling
             auto parser = with<error_handler_tag>(std::ref(error_handler))[ iniFile ];
 
+            // parse the input
             auto r = phrase_parse(iter, enditer, parser, ws);
 
+            // if the parsing failed, or we somehow didn't reach the end, throw an exception
             if (!r || iter != enditer)
             {
                 throw ParseError(err.str());
